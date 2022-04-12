930205b9eaab6d93819ada61f72064bf5c64ad83
Justen <98537182+alrightokguy@users.noreply.github.com>
2022-04-12 10:00:36 +0000

    qef

--- main.py
+++ main.py
@@ -1,29 +1,78 @@
 from flask import Flask, render_template, request
 app = Flask('app')
 
+
 @app.route('/')
 def splash():
   return render_template('splash.html')
 
-@app.route('/add', methods=['GET'])
-def add():
-    if page_type == 
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form_data = dict(request.form)
+    print(list(request.args))
+    
+    if len(request.args) == 0:
+        html = render_template('add.html',
+                               page_type='add')
+        return html
+        
+    elif 'club' in request.args:
+            html = render_template('add.html',
+                                   page_type='add_club',
+                                   form_meta={'action':'/add?confirm_club',
+                                              'method':'POST'},
+                                   form_data={'club_name':''})
+            
+    elif 'confirm_club' in request.args:
+        html = render_template('add.html',
+                               page_type='confirm_club',
+                               form_meta={'action':'/add?club_success',
+                                          'method':'POST'},
+                               form_data=form_data)
+
+    elif 'edit_club' in request.args:
+        html = render_template('add.html',
+                               page_type='add_club',
+                               form_meta={'action':'/add?edit_club',
+                                          'method':'POST'},
+                               form_data=form_data)
+
+    elif 'activity' in request.args:
+            html = render_template('add.html',
+                                   page_type='add_activity',
+                                   form_meta={'action':'/add?confirm_activity',
+                                              'method':'POST'},
+                                   form_data={'activity_name':'',
+                                              'start_date':'',
+                                              'end_date':'',
+                                              'desc':''})
+                                   
+    elif 'confirm_activity' in request.args:
+        html = render_template('add.html',
+                               page_type='confirm_activity',
+                               form_meta={'action':'/add?activity_success',
+                                          'method':'POST'},
+                               form_data=form_data)
 
     else:
-        html = render_template('form_student.html',
-                               page_type='new_club',
-                               form_meta={'action':'/add?new_club',
+        html = render_template('add.html',
+                               page_type='add_activity',
+                               form_meta={'action':'/add?edit_activity',
                                           'method':'POST'},
-                               form_data={'club_name':'',
-                                          'clubclub':''})
+                               form_data=form_data)
+
     return html
 
-@app.route('/view', methods=['GET'])
+
+
+
+
+@app.route('/view', methods=['GET', 'POST'])
 def view():
     return render_template('view.html')
 
-@app.route('/edit', methods=['GET'])
+@app.route('/edit', methods=['GET', 'POST'])
 def edit():
     return render_template('edit.html')
     
