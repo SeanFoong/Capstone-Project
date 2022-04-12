d86e58e1198c3273ea388c9bc61252db054690c2
Justen <98537182+alrightokguy@users.noreply.github.com>
2022-04-12 06:32:11 +0000

    ef

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask('app')
 
 @app.route('/')
-def hello_world():
-  return '<h1>Hello, World!</h1>'
+def splash():
+  return render_template('splash.html')
+
+@app_route('/add', methods=['POST'])
+def add_club():
+    form_data = dict(request.form)
 
 app.run(host='0.0.0.0', port=8080)
