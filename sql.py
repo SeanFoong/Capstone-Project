966e20c37fd8ae52c98f66e605f07ed29a0ce1f5
Unknownflow <81907539+Unknownflow@users.noreply.github.com>
2022-04-12 13:24:41 +0000

    inserted sql and __init__ functions for collections

---
+++ sql.py
@@ -0,0 +1,63 @@
+
+CREATE_STUDENT = """
+CREATE TABLE IF NOT EXISTS Student(
+    id INTEGER,
+    name TEXT,
+    age INTEGER,
+    year_enrolled INTEGER,
+    graduating_year INTEGER,
+    class INTEGER;
+    PRIMARY KEY(id),
+    FOREIGN KEY(class) REFERENCES Class(id)
+); """
+
+CREATE_CLASS = """
+CREATE TABLE IF NOT EXISTS Class(
+    id INTEGER,
+    name TEXT,
+    level TEXT CHECK (level in ('JC1', 'JC2'),
+    PRIMARY KEY(id)
+); """
+
+CREATE_SUBJECT = """
+CREATE TABLE IF NOT EXISTS Subject(
+    id INTEGER,
+    name TEXT CHECK (name in 'GP', 'MATH', 'FM', 'COMP', 'PHY', 'CHEM', 'ECONS', 'BIO', 'GEO', 'HIST', 'ELIT', 'ART', 'CLTRANS', 'CL', 'ML', 'TL', 'CLL', 'CLB', 'PW', 'PUNJABI', 'HINDI', 'BENGALESE', 'JAPANESE'),
+    level TEXT CHECK (level in 'H1', 'H2', 'H3'),
+    PRIMARY KEY(id)
+); """
+
+CREATE_CLUB = """
+CREATE TABLE IF NOT EXISTS Club(
+    id INTEGER,
+    name TEXT,
+    PRIMARY KEY(id)
+); """
+
+CREATE_ACTIVITY = """
+CREATE TABLE IF NOT EXISTS Activity(
+    id INTEGER,
+    start_date TEXT,
+    end_date TEXT,
+    description TEXT,
+    PRIMARY KEY(id)
+); """
+
+CREATE_STUDENT_SUBJECT = """
+CREATE TABLE IF NOT EXISTS Student_Subject(
+    student_id INTEGER,
+    subject_id INTEGER,
+    PRIMARY KEY(student_id, subject_id),
+    FOREIGN KEY(student_id) REFERENCES Student(id),
+    FOREIGN KEY(subject_id) REFERENCES Subject(id)
+); """
+
+CREATE_STUDENT_CLUB = """
+CREATE TABLE IF NOT EXISTS Student_Club(
+    student_id INTEGER,
+    club_id INTEGER,
+    PRIMARY KEY(student_id, club_id),
+    FOREIGN KEY(student_id) REFERENCES Student(id),
+    FOREIGN KEY(club_id) REFERENCES Club(id)
+); """
+
---
+++ storage.py
@@ -0,0 +1,125 @@
+import sqlite3
+import sql
+
+class Collections:
+    """
+    Encapsulates a collection of records.
+    """
+    # initialising the data table
+    def __init__(self):
+        pass
+
+    def execute(self, command, values=None):
+        with sqlite3.connect('nyjc_computing.db') as conn:
+            cur = conn.cursor()
+            if values == None:
+                cur.execute(command)
+            else:
+                cur.execute(command, values)
+        conn.commit()
+        # conn.close() is automatically called
+
+    
+class Student(Collections):
+    """
+    Encapsulates a collection of student records.
+    Each record has the following columns:
+    - Name: str
+    - Age: int
+    - Year enrolled: int
+    - Graduating year: int
+    """
+    table = 'Student'
+    # initialising the data table for students
+    def __init__(self, db_uri = 'nyjc_computing.db'):
+        # with sqlite3.connect(db_uri) as conn:
+        #     cur = conn.cursor()
+        #     cur.execute(sql.CREATE_STUDENT)
+        #     conn.commit()
+        # conn.close() is automatically called
+
+        self.execute(sql.CREATE_STUDENT)
+
+
+class Class(Collections):
+    """
+    Encapsulates a collection of class records.
+    Each record has the following columns:
+    - Name: str
+    - Level: str {JC1, JC2}
+    """
+    table = 'Class'
+    # initialising the data table for class
+    def __init__(self, db_uri = 'nyjc_computing.db'):
+        # with sqlite3.connect(db_uri) as conn:
+        #     cur = conn.cursor()
+        #     cur.execute(sql.CREATE_CLASS)
+        #     conn.commit()
+        # conn.close() is automatically called
+
+        self.execute(sql.CREATE_CLASS)
+            
+class Subject(Collections):
+    """
+    Encapsulates a collection of subject records.
+    Each record has the following columns:
+    - Name: str {GP, MATH, FM, COMP, PHY, CHEM, ECONS, BIO, GEO, HIST, ELIT, ART, CLTRANS, CL, ML, TL, CLL, CLB, PW, PUNJABI, HINDI, BENGALESE, JAPANESE}
+    - Level: str {H1, H2, H3}
+    """
+    table = 'Subject'
+    # initialising the data table for class
+    def __init__(self, db_uri = 'nyjc_computing.db'):
+        # with sqlite3.connect(db_uri) as conn:
+        #     cur = conn.cursor()
+        #     cur.execute(sql.CREATE_SUBJECT)
+        #     conn.commit()
+        # conn.close() is automatically called
+
+        self.execute(sql.CREATE_SUBJECT)
+    
+class Club(Collections):
+    """
+    Encapsulates a collection of club records.
+    Each record has the following columns:
+    - Name: str 
+    """
+    table = 'Club'
+    # initialising the data table for class
+    def __init__(self, db_uri = 'nyjc_computing.db'):
+        # with sqlite3.connect(db_uri) as conn:
+        #     cur = conn.cursor()
+        #     cur.execute(sql.CREATE_CLUB)
+        #     conn.commit()
+        # conn.close() is automatically called
+
+        self.execute(sql.CREATE_CLUB)
+    
+class Activity(Collections):
+    """
+    Encapsulates a collection of student records.
+    Each record has the following columns:
+    - Start Date: str (ISO8601 date)
+    - End Date: str (optional)
+    - Description: str
+    """
+    table = 'Activity'
+    # initialising the data table for class
+    def __init__(self, db_uri = 'nyjc_computing.db'):
+        # with sqlite3.connect(db_uri) as conn:
+        #     cur = conn.cursor()
+        #     cur.execute(sql.CREATE_ACTIVITY)
+        #     conn.commit()
+        # conn.close() is automatically called
+
+        self.execute(sql.CREATE_ACTIVITY)
+    
+
+        
+
+
+# for record in data:
+#     cur.execute(
+#         """INSERT INTO Subject VALUES(?, ?, ?),
+#         tuple(record.values())
+#         """)
+    
