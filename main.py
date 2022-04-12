1f2f5ee125b0fe2b6071c7cddff8bda82b1a2af8
Justen <98537182+alrightokguy@users.noreply.github.com>
2022-04-12 11:40:37 +0000

    sc

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def add():
     if len(request.args) == 0:
         html = render_template('add.html',
                                page_type='add')
-        return html
         
     elif 'club' in request.args:
             html = render_template('add.html',
@@ -69,13 +68,11 @@ def add():
                                name=name)
     return html
 
-@app.route('/')
-
-
-
+    
 @app.route('/view', methods=['GET', 'POST'])
 def view():
     return render_template('view.html')
+
 
 @app.route('/edit', methods=['GET', 'POST'])
 def edit():
