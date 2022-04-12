b6588dd7a40d8866609322d2a27f464757572ae8
SEAN FOONG JER TSUEN FOONG <98528956+SeanFoong@users.noreply.github.com>
2022-04-12 06:05:57 +0000

    Basic page routes

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ app = Flask('app')
 
 @app.route('/')
 def hello_world():
-  return '<h1>Hello, World!</h1>'
+  return render_template('splash.html')
+
+@app.route('/new_student', )
+def new_student():
+  if confirm in request.args()
+
+  else:
+    return render_template('')
+
+@app.route('/')
 
 app.run(host='0.0.0.0', port=8080)
