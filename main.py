56b91473b93f1fd4361440bfb0686ac03b845750
Justen <98537182+alrightokguy@users.noreply.github.com>
2022-04-12 06:55:45 +0000

    web man

--- main.py
+++ main.py
@@ -5,8 +5,26 @@ app = Flask('app')
 def splash():
   return render_template('splash.html')
 
-@app_route('/add', methods=['POST'])
-def add_club():
-    form_data = dict(request.form)
+@app.route('/add', methods=['GET'])
+def add():
+    if page_type == 
 
+
+    else:
+        html = render_template('form_student.html',
+                               page_type='new_club',
+                               form_meta={'action':'/add?new_club',
+                                          'method':'POST'},
+                               form_data={'club_name':'',
+                                          'clubclub':''})
+    return html
+
+@app.route('/view', methods=['GET'])
+def view():
+    return render_template('view.html')
+
+@app.route('/edit', methods=['GET'])
+def edit():
+    return render_template('edit.html')
+    
 app.run(host='0.0.0.0', port=8080)
