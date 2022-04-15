2fd5aa6160f9c4326e3f1954d55dd14fc12b9ac3
Justen <98537182+alrightokguy@users.noreply.github.com>
2022-04-15 17:37:50 +0000

    c

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ def add():
  
     if len(request.args) == 0:
         html = render_template('add.html',
-                               page_type='add')
+                               page_type='add',
+                               form_meta={'action':'/add?new',
+                                          'method':'POST'})
         
     elif 'new' in request.args:
         if form_data['type'] == 'Club':
@@ -55,7 +57,29 @@ def add():
     
 @app.route('/view', methods=['GET', 'POST'])
 def view():
-    return render_template('view.html')
+    form_data = dict(request.form)
+    
+    if len(request.args) == 0:
+        html = render_template('view.html',
+                               page_type='view',
+                              )
+        
+    elif 'search' in request.args:
+        html = render_template('view.html',
+                               page_type='search',
+                               form_meta={'action':'/view?result',
+                                          'method':'POST'},
+                               form_data=form_data)
+
+    elif 'result' in request.args:
+        # search database here
+        # form_data[''] = 
+        
+        html = render_template('view.html',
+                               page_type='result',
+                               form_data=form_data)
+
+    return html
 
 
 @app.route('/edit', methods=['GET', 'POST'])
